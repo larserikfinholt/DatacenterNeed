dba96b38a240c97d422bd67b75f75d867924ade9
Lars Erik Finholt <lars.erik.finholt@yara.com>
2026-09-10 14:23:20 +0200

    Deploy dashboard to GitHub Pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/DatacenterNeed/',
   server: {
     fs: {
       allow: ['.', '../data/norway', '../docs'].map((path) => fileURLToPath(new URL(path, import.meta.url))),
